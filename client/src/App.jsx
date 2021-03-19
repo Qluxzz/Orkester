3a46a759904f022841c42e0f4e00590ed90f4cd3
Qluxzz <andre.eric.andersson@gmail.com>
2021-03-20 00:29:44 +0100

    Restructure

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <audio
+        controls
+        src="http://localhost:3000/stream"
+      />
+    </div>
+  );
+}
+
+export default App;
