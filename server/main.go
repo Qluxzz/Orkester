a20aead2f88090366c2a753ba4ba16777c63ce97
Qluxzz <andre.eric.andersson@gmail.com>
2021-03-22 21:55:16 +0100

    Add indexing of tracks and expose tracks via route

--- server/main.go
+++ server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jmoiron/sqlx"
@@ -15,122 +14,59 @@ import (
 )
 
 func createSchemas(db *sqlx.DB) {
-	artistSchema := `CREATE TABLE IF NOT EXISTS artist (
+	trackSchema := `CREATE TABLE IF NOT EXISTS track(
 		id INTEGER PRIMARY KEY,
-		name TEXT NOT NULL
+		path TEXT NOT NULL
 	);`
 
-	_, err := db.Exec(artistSchema)
+	_, err := db.Exec(trackSchema)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	genreSchema := `CREATE TABLE IF NOT EXISTS genre (
-		id INTEGER PRIMARY KEY,
-		name TEXT NOT NULL
-	);`
-
-	_, err = db.Exec(genreSchema)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	albumSchema := `CREATE TABLE IF NOT EXISTS album (
-		id INTEGER PRIMARY KEY,
-		name TEXT NOT NULL,
-		date TEXT NOT NULL
-	);`
-
-	_, err = db.Exec(albumSchema)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	albumArtistsSchema := `CREATE TABLE IF NOT EXISTS albumartist (
-		albumid INTEGER NOT NULL,
-		artistid INTEGER NOT NULL,
-		FOREIGN KEY (albumid) REFERENCES album(id),
-		FOREIGN KEY (artistid) REFERENCES artist(id)
-	);`
-
-	_, err = db.Exec(albumArtistsSchema)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	albumGenresSchema := `CREATE TABLE IF NOT EXISTS albumgenre (
-		albumid INTEGER NOT NULL,
-		genreid INTEGER NOT NULL,
-		FOREIGN KEY (albumid) REFERENCES album(id),
-		FOREIGN KEY (genreid) REFERENCES genre(id)
-	);`
-
-	_, err = db.Exec(albumGenresSchema)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	trackSchema := `CREATE TABLE IF NOT EXISTS track (
-		id INTEGER PRIMARY KEY,
-		name text NOT NULL,
-		path TEXT NOT NULL,
-		albumid INTEGER NOT NULL,
-		FOREIGN KEY (albumid) REFERENCES album(id)
-	);`
-
-	_, err = db.Exec(trackSchema)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	trackArtistsSchema := `CREATE TABLE IF NOT EXISTS trackartist (
-		trackid INTEGER NOT NULL,
-		artistid INTEGER NOT NULL,
-		FOREIGN KEY (trackid) REFERENCES track(id),
-		FOREIGN KEY (artistid) REFERENCES artist(id)
-	);`
-
-	_, err = db.Exec(trackArtistsSchema)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 }
 
-type Artist struct {
-	Id   int
-	Name string
+type AddTrackRequest struct {
+	Path string `db:"path"`
 }
 
-type Album struct {
-	Id   int
-	Name string
-	Date time.Time
-}
+func indexFolder(path string) []AddTrackRequest {
+	path, err := filepath.EvalSymlinks(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-func indexFolder(path string) {
-	filepath.Walk(path, walkFn)
-}
+	tracks := []AddTrackRequest{}
 
-func walkFn(path string, fileInfo os.FileInfo, err error) error {
-	if fileInfo.IsDir() {
+	filepath.Walk(path, func(path string, fileInfo os.FileInfo, err error) error {
+		filename := strings.ToLower(fileInfo.Name())
+		ext := filepath.Ext(filename)
+
+		if isMusicFile(ext) {
+			tracks = append(
+				tracks,
+				AddTrackRequest{
+					Path: path,
+				},
+			)
+
+			return nil
+		}
+
+		if isCoverImage(filename) {
+			return nil
+		}
+
 		return nil
-	}
+	})
 
-	if !isMusicFile(fileInfo.Name()) {
-		return nil
-	}
-
-	log.Printf("Found music file: %s", path)
-
-	return nil
+	return tracks
 }
 
-func isMusicFile(filename string) bool {
-	validFileExtensions := []string{"ogg", "flac", "mp3"}
+func isMusicFile(extension string) bool {
+	validFileExtensions := []string{".ogg", ".flac", ".mp3"}
 
-	for _, extension := range validFileExtensions {
-		if strings.HasSuffix(filename, extension) {
+	for _, validExtension := range validFileExtensions {
+		if extension == validExtension {
 			return true
 		}
 	}
@@ -138,13 +74,55 @@ func isMusicFile(filename string) bool {
 	return false
 }
 
+func isCoverImage(filename string) bool {
+	hasValidFileName := func() bool {
+		validFilenames := []string{"cover", "folder"}
+
+		for _, validFilename := range validFilenames {
+			if strings.HasPrefix(filename, validFilename) {
+				return true
+			}
+		}
+
+		return false
+	}()
+
+	hasValidExtension := func() bool {
+		validFileExtensions := []string{".jpg", ".jpeg", ".png"}
+
+		for _, validFileExtension := range validFileExtensions {
+			if strings.HasSuffix(filename, validFileExtension) {
+				return true
+			}
+		}
+
+		return false
+	}()
+
+	return hasValidFileName && hasValidExtension
+}
+
+type DBTrack struct {
+	Id   int    `db:"id"`
+	Path string `db:"path"`
+}
+
+func addTracks(tracks []AddTrackRequest, db *sqlx.DB) {
+	log.Printf("Tracks found: %d", len(tracks))
+
+	for _, track := range tracks {
+		res := db.MustExec("INSERT INTO track (path) VALUES (?)", track.Path)
+		last, err := res.LastInsertId()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Print(last)
+	}
+}
+
 func main() {
-
-	indexFolder("./content")
-
-	return
-
-	db, err := sqlx.Open("sqlite3", ":memory:")
+	db, err := sqlx.Connect("sqlite3", ":memory:")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -153,54 +131,46 @@ func main() {
 
 	createSchemas(db)
 
+	tracks := indexFolder("./content")
+	addTracks(tracks, db)
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		db, err := sqlx.Open("sqlite3", ":memory:")
+		tracks := []DBTrack{}
+		db.Select(&tracks, "SELECT * FROM track")
+
+		_json, err := json.Marshal(tracks)
 		if err != nil {
-			log.Fatalln(err)
+			return c.Status(500).SendString(err.Error())
 		}
 
-		defer db.Close()
+		c.Response().Header.Add("Content-Type", "application/json")
 
-		db.Queryx("SELECT * FROM tracks")
-
-		return c.SendString("Hello, World 👋!")
+		return c.Send(_json)
 	})
 
-	app.Get("/stream/:type/:id", func(c *fiber.Ctx) error {
+	app.Get("/stream/:id", func(c *fiber.Ctx) error {
 		id, err := strconv.Atoi(c.Params("id"))
 		if err != nil {
-			log.Fatalln("Invalid id: ", id)
+			return c.Status(500).SendString(err.Error())
 		}
 
-		_type := c.Params("type")
-
-		switch _type {
-		case "album":
-			getAlbum(db, id)
-		case "artist":
-			getArtist(db, id)
-		case "track":
-			getTrack(db, id)
+		track := DBTrack{}
+		err = db.Get(&track, "SELECT * FROM track WHERE id=:id", id)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
 		}
 
-		//c.Response().Header.Add("content-type", "audio/ogg")
+		c.Response().Header.Add("content-type", "audio/flac")
 
-		return c.SendString(fmt.Sprintf("Fetching %s with id %d", _type, id))
+		stream, err := os.Open(track.Path)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		return c.SendStream(stream)
 	})
 
 	app.Listen(":3000")
-}
-
-func getAlbum(db *sqlx.DB, id int) {
-	log.Println("Fetching album with id:", id)
-}
-
-func getArtist(db *sqlx.DB, id int) {
-	log.Println("Fetching artist with id:", id)
-}
-
-func getTrack(db *sqlx.DB, id int) {
-	log.Println("Fetching track with id:", id)
 }
