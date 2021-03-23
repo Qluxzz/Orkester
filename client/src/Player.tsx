061d3d8126ea0d5d7961187547b629abcd732fba
Qluxzz <andre.eric.andersson@gmail.com>
2021-03-23 23:52:42 +0100

    Add track detail view

--- client/src/Player.tsx
+++ client/src/Player.tsx
@@ -1,42 +1,64 @@
 import React, { useState, useEffect } from "react"
-import IPlaybackState from "./types/playbackState"
-import PlaybackButton from "./PlaybackButton"
+import ITrack from "./types/track"
+import styled from "styled-components"
+
+
+const PlayerDiv = styled.div`
+    background-radius: 20%;
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+`
+
 
 export default function Player({ id }: { id: number }) {
-    const [playbackState, setPlaybackState] = useState<IPlaybackState>("paused")
-    const audio = new Audio(`http://localhost:3000/stream/${id}`)
+    const [track, setTrack] = useState<ITrack | "loading">("loading")
 
     useEffect(() => {
-        async function togglePlayback() {
-            switch (playbackState) {
-                case "paused":
-                    if (!audio.paused)
-                        audio.pause()
-                    break
-                case "playing":
-                    await audio.play()
-                    break
+        let isCanceled = false
+
+        async function fetchTrackDetails() {
+            try {
+                const response = await fetch(`http://localhost:3001/track/${id}`)
+                if (!response.ok)
+                    throw new Error(`Http request failed with status code: ${response.status}`)
+
+                const trackInfo: ITrack = await response.json()
+
+                return trackInfo
+            } catch (error) {
+                console.log("Failed to get track info!", error)
+                throw error
             }
         }
 
-        togglePlayback()
-    }, [playbackState])
+        fetchTrackDetails()
+            .then(track => {
+                if (!isCanceled)
+                    setTrack(track)
+            })
+            .catch(error => console.error(error))
 
-    function togglePlayback() {
-        setPlaybackState(
-            currentState => {
-                switch (currentState) {
-                    case "playing":
-                        return "paused"
-                    case "paused":
-                        return "playing"
-                }
-            }
-        )
-    }
+        return () => { isCanceled = true }
+    }, [id])
 
-    return <PlaybackButton
-        playbackState={playbackState}
-        togglePlayback={togglePlayback}
+    if (track === "loading")
+        return <div>Loading</div>
+
+    return <PlayerDiv>
+        <h2>{track.Title}</h2>
+        <p>{track.Artist} - {track.Album}</p>
+        <img style={{ padding: 10, width: 256 }} src={`http://localhost:3001/track/${id}/image`} alt={track.Album} />
+        <Controls id={id} />
+    </PlayerDiv>
+}
+
+function Controls({ id }: { id: number }) {
+    return <audio
+        src={`http://localhost:3001/stream/${id}`}
+        controls
     />
 }
