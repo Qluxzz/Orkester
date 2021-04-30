caa6758d489259fbb010a8a7a8850bb8dd7bcc93
Qluxzz <andre.eric.andersson@gmail.com>
2021-04-30 18:04:39 +0200

    Restructure project and set baseUrl

--- web/src/Features/Player/PlaybackButton.tsx
+++ web/src/Features/Player/PlaybackButton.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import IPlaybackState from "./types/playbackState"
+import IPlaybackState from "../../types/playbackState"
 
 type IButton = {
     playbackState: IPlaybackState
--- web/src/Features/Player/PlayerBar.tsx
+++ web/src/Features/Player/PlayerBar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
-import { usePlayerContext } from "./Context"
+import { usePlayerContext } from "../../Context"
 
 const Bar = styled.div`
   display: flex;
