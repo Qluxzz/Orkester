4e309ccd9d3d5d892857edf44311303c71c16bb3
Qluxzz <andre.eric.andersson@gmail.com>
2021-05-11 22:57:59 +0200

    Don't show loader until after 500 ms

--- web/src/CenteredDotLoader.tsx
+++ web/src/CenteredDotLoader.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DotLoader } from "react-spinners";
 
 export default function CenteredDotLoader() {
+    const [show, setShow] = useState(false)
+    const showLoaderTimeout = 500
+
+    useEffect(() => {
+        let isCanceled = false
+
+        const timeoutId = setTimeout(() => {
+            if (isCanceled) {
+                clearTimeout(timeoutId)
+                return
+            }
+
+            setShow(true)
+        }, showLoaderTimeout)
+
+        return () => { isCanceled = true }
+    }, [])
+
+    if (!show)
+        return null
+
     return <div style={{
         display: "flex",
         justifyContent: "center",
