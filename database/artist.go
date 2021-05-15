0489f9555a3b37a1d257df5457a88a3be5d44c1a
Qluxzz <andre.eric.andersson@gmail.com>
2021-05-15 22:33:55 +0200

    Don't expose everything

--- database/artist.go
+++ database/artist.go
@@ -1,23 +1,20 @@
 package database
 
 import (
-	"goreact/models"
-
 	"github.com/jmoiron/sqlx"
 )
 
-type ArtistAlbum struct {
-	Id      int            `json:"id"`
-	Name    string         `json:"name"`
-	UrlName string         `json:"urlName"`
-	Tracks  []models.Track `json:"tracks"`
+type artistAlbum struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	UrlName string `json:"urlName"`
 }
 
 type artist struct {
 	Id      int           `db:"id" json:"id"`
 	Name    string        `db:"name" json:"name"`
 	UrlName string        `db:"urlname" json:"urlName"`
-	Albums  []ArtistAlbum `json:"albums"`
+	Albums  []artistAlbum `json:"albums"`
 }
 
 func GetArtistById(artistId int, db *sqlx.DB) (*artist, error) {
@@ -33,10 +30,8 @@ func GetArtistById(artistId int, db *sqlx.DB) (*artist, error) {
 		return nil, err
 	}
 
-	albums := []ArtistAlbum{}
-
 	err = db.Select(
-		&albums,
+		&artist.Albums,
 		`SELECT
 				id,
 				name,
@@ -52,8 +47,6 @@ func GetArtistById(artistId int, db *sqlx.DB) (*artist, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	artist.Albums = albums
 
 	return &artist, nil
 }
