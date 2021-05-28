dd00109e457c2dbd9541509cca62a5984b82b801
Qluxzz <andre.eric.andersson@gmail.com>
2021-05-28 21:10:41 +0200

    Move player from playerbar to context

--- web/src/Context.tsx
+++ web/src/Context.tsx
@@ -1,24 +1,22 @@
-import React, { useState, useContext, useEffect } from "react"
+import React, { useState, useContext, useEffect, useRef, useCallback, useMemo } from "react"
+import IPlaybackState from "types/playbackState"
 import ITrack from "./types/track"
 
 interface IPlayerContext {
     track?: ITrack
     play: (id: number) => void
+    togglePlayback: () => void
+    state: IPlaybackState
+    player?: HTMLAudioElement
 }
 
 const PlayerContext = React.createContext<IPlayerContext>({
     track: undefined,
-    play: () => { throw new Error("Tried to access context outside of provider") }
+    play: () => { throw new Error("Tried to access context outside of provider") },
+    togglePlayback: () => { throw new Error("Tried to access context outside of provider") },
+    state: "paused",
+    player: undefined
 })
-
-/*
- Track loading lifecycle,
- Inital value is undefined, there's no track at all, possibly one could store a track id in local storage that is fetched whenever the site is launched
- Then while the track is loading, it's status is set to loading
- If the request succedded, loading-finished and track is set correctly
- If the request failed, loading-failed with a modal or notification that the track failed to load
- 
-*/
 
 export const usePlayerContext = () => useContext(PlayerContext)
 
@@ -32,36 +30,88 @@ async function fetchTrackDetails(id: number) {
     return track
 }
 
+interface ILocalStorageTrack {
+    id: number
+    timestamp: number
+}
+
 
 export function PlayerContextProvider({ children }: { children: React.ReactNode }) {
     const [track, setTrack] = useState<ITrack>()
+    const [state, setState] = useState<IPlaybackState>("paused")
 
-    useEffect(() => {
-        const savedTrack = localStorage.getItem("trackId")
-        if (!savedTrack)
-            return
+    const playerRef = useRef(new Audio())
+    const player = playerRef.current
 
-        const trackId = parseInt(savedTrack)
-
-        if (!trackId)
-            return
-
-        loadTrack(trackId)
-    }, [])
-
-    const loadTrack = (id: number) => {
+    const loadTrack = useCallback((id: number) =>
         fetchTrackDetails(id)
             .then(track => {
                 setTrack(track)
-                localStorage.setItem("trackId", track.id.toString())
+                localStorage.setItem("track", JSON.stringify({ id: track.id, timestamp: 0 }))
                 document.title = `${track.title} - ${track.artists.map(artist => artist.name).join(", ")}`
+                player.src = `/api/v1/track/${track.id}/stream`
+                player.play()
+                setState("playing")
             })
             .catch(error => {
                 console.error("Something went wrong while loading track info", error)
             })
-    }
+        , [player])
 
-    return <PlayerContext.Provider value={{ track, play: loadTrack }}>
+    const togglePlayBack = useCallback(async () => {
+        if (player.paused) {
+            await player.play()
+            setState("playing")
+        } else {
+            player.pause()
+            setState("paused")
+        }
+    }, [player])
+
+    useEffect(() => {
+        const savedTrackJson = localStorage.getItem("track")
+        if (!savedTrackJson)
+            return
+
+        const { id, timestamp }: ILocalStorageTrack = JSON.parse(savedTrackJson)
+        if (!id)
+            return
+
+        loadTrack(id)
+            .then(_ => {
+                player.fastSeek(timestamp)
+            })
+    }, [loadTrack, player])
+
+    useEffect(() => {
+        if (!track)
+            return
+
+        if (state !== "playing")
+            return
+
+        const interval = setInterval(
+            () => {
+                localStorage.setItem("track", JSON.stringify({ id: track.id, timestamp: player.currentTime }))
+            },
+            1000
+        )
+
+        return () => { clearInterval(interval) }
+    }, [track, state])
+
+
+    const memoValues = useMemo(() => ({
+        track: track,
+        play: loadTrack,
+        togglePlayback: togglePlayBack,
+        state: state,
+        player: player
+    }), [loadTrack, player, state, togglePlayBack, track])
+
+    return <PlayerContext.Provider
+        value={memoValues}
+    >
         {children}
     </PlayerContext.Provider>
 }
