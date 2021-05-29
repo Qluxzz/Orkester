49258bbcccd33e1ee609ecca61ff4f29acd3cd0c
Qluxzz <andre.eric.andersson@gmail.com>
2021-05-29 21:41:25 +0200

    Make album image to own component

---
+++ web/src/utilities/AlbumImage.tsx
@@ -0,0 +1,8 @@
+interface IAlbumImage {
+    album: { id: number, name: string }
+    size: string | number
+}
+
+export default function AlbumImage({ album: { id, name }, size }: IAlbumImage) {
+    return <img width={size} height={size} src={`/api/v1/album/${id}/image`} alt={name} />
+}
