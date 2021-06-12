14287ad3ec58fe3c459ea31f99fc1fc41e41ac7c
Qluxzz <andre.eric.andersson@gmail.com>
2021-06-12 12:35:17 +0200

    Optimize search by only selecting needed fields

--- handlers/search.go
+++ handlers/search.go
@@ -14,17 +14,52 @@ func Search(client *ent.Client, context context.Context) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		query := c.Params("query")
 
-		tracks, err := client.Track.Query().Where(track.TitleContains(query)).All(context)
+		var tracks []struct {
+			Id    int    `json:"id"`
+			Title string `json:"title"`
+		}
+
+		err := client.
+			Track.
+			Query().
+			Where(track.TitleContains(query)).
+			Select(track.FieldID, track.FieldTitle).
+			Scan(context, &tracks)
+
 		if err != nil {
 			return err
 		}
 
-		albums, err := client.Album.Query().Where(album.NameContains(query)).All(context)
+		var albums []struct {
+			Id      int    `json:"id"`
+			Name    string `json:"name"`
+			URLName string `json:"urlName" sql:"url_name"`
+		}
+
+		err = client.
+			Album.
+			Query().
+			Where(album.NameContains(query)).
+			Select(album.FieldID, album.FieldName, album.FieldURLName).
+			Scan(context, &albums)
+
 		if err != nil {
 			return err
 		}
 
-		artists, err := client.Artist.Query().Where(artist.NameContains(query)).All(context)
+		var artists []struct {
+			Id      int    `json:"id"`
+			Name    string `json:"name"`
+			URLName string `json:"urlName"  sql:"url_name"`
+		}
+
+		err = client.
+			Artist.
+			Query().
+			Where(artist.NameContains(query)).
+			Select(artist.FieldID, artist.FieldName, artist.FieldURLName).
+			Scan(context, &artists)
+
 		if err != nil {
 			return err
 		}
