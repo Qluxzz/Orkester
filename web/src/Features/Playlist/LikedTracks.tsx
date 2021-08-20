ec6d783ade4f0af251e05dc9f00cb8da465a3d18
André Andersson <andre.eric.andersson@gmail.com>
2021-08-20 15:13:02 +0200

    Fix layout

--- web/src/Features/Playlist/LikedTracks.tsx
+++ web/src/Features/Playlist/LikedTracks.tsx
@@ -53,8 +53,7 @@ export default function LikedTracks() {
                 columns={[
                     { display: "#", key: "index", width: 50 },
                     { display: "", key: "albumCover" },
-                    { display: "TITLE", key: "title", width: 400 },
-                    { display: "ALBUM", key: "album", width: "grow" },
+                    { display: "TITLE", key: "title", width: "grow" },
                     { display: "ADDED", key: "date", width: 100, centered: true },
                     { display: "", key: "liked", width: 100, centered: true },
                     { display: "🕒", key: "length", width: 60, centered: true }
