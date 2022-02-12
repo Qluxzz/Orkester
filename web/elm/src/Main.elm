f266445327d8043e91dc2267136c104d9e4abf15
André Andersson <andre.eric.andersson@gmail.com>
2022-02-12 12:24:28 +0100

    Add dummy data for search result

--- web/elm/src/Main.elm
+++ web/elm/src/Main.elm
@@ -2,19 +2,53 @@ module Main exposing (..)
 
 import Browser
 import Css exposing (..)
-import Css.Global exposing (Snippet)
+import Css.Global
 import Html.Events exposing (..)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css, type_)
 
 
+type alias IdNameAndUrlName =
+    { id : Int
+    , name : String
+    , urlName : String
+    }
+
+
+type alias Album =
+    IdNameAndUrlName
+
+
+type alias Artist =
+    IdNameAndUrlName
+
+
+type alias Track =
+    { id : Int
+    , title : String
+    }
+
+
+type alias SearchResult =
+    { albums : List Album
+    , artists : List Artist
+    , tracks : List Track
+    }
+
+
 type alias Model =
-    {}
+    { searchResult : SearchResult
+    }
 
 
 init : Model
 init =
-    {}
+    { searchResult =
+        { albums = [ { id = 1, name = "Maniac", urlName = "maniac" } ]
+        , artists = [ { id = 1, name = "Carpenter Brut", urlName = "carpenter-brut" } ]
+        , tracks = []
+        }
+    }
 
 
 globalStyle : Html msg
@@ -39,8 +73,12 @@ view model =
             [ css [ displayFlex, flexDirection row, backgroundColor (hex "#222"), height (pct 100) ] ]
             [ aside [ css [ padding (px 10), backgroundColor (hex "#333"), width (px 200) ] ] [ text "Sidebar" ]
             , section [ css [ displayFlex, flexDirection column, padding (px 20), flexGrow (int 1) ] ]
-                [ div [ css [ marginBottom (px 20), displayFlex ] ]
+                [ div [ css [ marginBottom (px 20), displayFlex, flexDirection column ] ]
                     [ input [ css [ flexGrow (int 1) ], type_ "text" ] []
+                    , div [ css [ displayFlex ] ]
+                        [ ul [ css [ flexGrow (int 1) ] ] (List.map viewSearchResultEntry model.searchResult.albums)
+                        , ul [ css [ flexGrow (int 1) ] ] (List.map viewSearchResultEntry model.searchResult.artists)
+                        ]
                     ]
                 , div [] [ text "Main content" ]
                 ]
@@ -49,6 +87,11 @@ view model =
             [ text "Nothing is currently playing..."
             ]
         ]
+
+
+viewSearchResultEntry : { a | id : Int, name : String, urlName : String } -> Html Msg
+viewSearchResultEntry entry =
+    li [] [ text entry.name ]
 
 
 type Msg
