9b292fab1191667af207a086b2b174e55e236552
Qluxzz <andre.eric.andersson@gmail.com>
2022-09-09 22:49:33 +0200

    Fix overflow

--- web/elm/src/QueueView.elm
+++ web/elm/src/QueueView.elm
@@ -1,17 +1,19 @@
 module QueueView exposing (..)
 
+import Css exposing (auto, overflow, position, px, sticky, top)
 import Html.Styled exposing (Html, div, h1, li, text, ul)
+import Html.Styled.Attributes exposing (css)
 import Queue exposing (Queue, getCurrent, getFuture, getHistory)
 import TrackId exposing (TrackId)
 
 
 queueView : Queue TrackId -> Html msg
 queueView queue =
-    div []
-        [ h1 [] [ text "History" ]
+    div [ css [ overflow auto ] ]
+        [ h1 [ css [ position sticky, top (px 0) ] ] [ text "History" ]
         , ul [] (List.map (\tId -> li [] [ text (String.fromInt tId) ]) (getHistory queue))
-        , h1 [] [ text "Now playing" ]
+        , h1 [ css [ position sticky, top (px 0) ] ] [ text "Now playing" ]
         , ul [] [ li [] [ text (Maybe.withDefault "" (queue |> getCurrent |> Maybe.map (\c -> String.fromInt c))) ] ]
-        , h1 [] [ text "Coming next" ]
+        , h1 [ css [ position sticky, top (px 0) ] ] [ text "Coming next" ]
         , ul [] (List.map (\tId -> li [] [ text (String.fromInt tId) ]) (getFuture queue))
         ]
