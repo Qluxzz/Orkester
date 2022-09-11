def52d7aee0bec1ac9f1c062c367b34e03eb9b8b
Qluxzz <andre.eric.andersson@gmail.com>
2022-09-11 12:01:48 +0200

    Style queue view

--- web/src/QueueView.elm
+++ web/src/QueueView.elm
@@ -1,7 +1,7 @@
 module QueueView exposing (..)
 
-import Css exposing (auto, bottom, listStyle, none, overflow, padding, position, px, sticky, top)
-import Html.Styled exposing (Html, div, h1, li, text, ul)
+import Css exposing (auto, bold, bottom, fontSize, fontWeight, listStyle, none, overflow, padding, position, px, sticky, top)
+import Html.Styled exposing (Html, div, h1, li, p, text, ul)
 import Html.Styled.Attributes exposing (css)
 import Queue exposing (Queue, getCurrent, getFuture, getHistory)
 import TrackInfo exposing (Track)
@@ -12,17 +12,20 @@ styledList =
     ul [ css [ listStyle none, padding (px 0) ] ]
 
 
+styledP : List (Html msg) -> Html msg
+styledP =
+    p [ css [ fontSize (px 20), fontWeight bold ] ]
+
+
 queueView : Queue Track -> Html msg
 queueView queue =
     div [ css [ overflow auto ] ]
-        [ h1 [ css [ position sticky, top (px 0) ] ] [ text "History" ]
+        [ styledP [ text "History" ]
         , styledList (List.map (\{ title } -> li [] [ text title ]) (getHistory queue))
-        , h1
-            [ css [ position sticky, top (px 0), bottom (px 0) ] ]
+        , styledP
             [ text "Now playing" ]
         , styledList [ li [] [ text (Maybe.withDefault "" (queue |> getCurrent |> Maybe.map (\{ title } -> title))) ] ]
-        , h1
-            [ css [ position sticky, top (px 0), bottom (px 0) ] ]
+        , styledP
             [ text "Coming next" ]
         , styledList (List.map (\{ title } -> li [] [ text title ]) (getFuture queue))
         ]
