df24c86f6d92eeb8afd8f06816028d9299662227
Qluxzz <andre.eric.andersson@gmail.com>
2024-12-03 22:30:20 +0100

    Base layout

---
+++ web2/src/Layouts/Default.elm
@@ -0,0 +1,76 @@
+module Layouts.Default exposing (Model, Msg, Props, layout)
+
+import Effect exposing (Effect)
+import Html exposing (Html)
+import Html.Attributes exposing (class)
+import Layout exposing (Layout)
+import Route exposing (Route)
+import Shared
+import View exposing (View)
+
+
+type alias Props =
+    {}
+
+
+layout : Props -> Shared.Model -> Route () -> Layout () Model Msg contentMsg
+layout props shared route =
+    Layout.new
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init _ =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReplaceMe
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model
+            , Effect.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : { toContentMsg : Msg -> contentMsg, content : View contentMsg, model : Model } -> View contentMsg
+view { toContentMsg, model, content } =
+    { title = content.title
+    , body = 
+        [ Html.aside [ Html.Attributes.class "side-bar" ] [ Html.text "Sidebar"]
+        , Html.main_ [] content.body
+        , Html.aside [ Html.Attributes.class "queue" ] [ Html.text "Queue"]
+        , Html.div [ Html.Attributes.class "player-bar"] [ Html.text "player bar"]
+        ]
+    }
--- web2/src/Pages/Home_.elm
+++ web2/src/Pages/Home_.elm
@@ -1,11 +1,75 @@
-module Pages.Home_ exposing (page)
+module Pages.Home_ exposing (Model, Msg, page)
 
+import Effect exposing (Effect)
+import Route exposing (Route)
 import Html
+import Page exposing (Page)
+import Shared
 import View exposing (View)
+import Layouts
 
 
-page : View msg
-page =
-    { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+page : Shared.Model -> Route () -> Page Model Msg
+page shared route =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+        |> Page.withLayout toLayout
+
+
+toLayout : Model -> Layouts.Layout Msg
+toLayout model =
+    Layouts.Default {}
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Effect.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Orkester"
+    , body = [ Html.text "Welcome!" ]
     }
